bdd6b608edbd77153fbafcb8dda215cf87f2f886
Zaki Manian <zaki@manian.org>
2021-02-04 18:50:40 -0800

    Remove an only

--- solidity/tests_mainnet_fork/uniswap-logic.ts
+++ solidity/tests_mainnet_fork/uniswap-logic.ts
@@ -191,7 +191,7 @@ let usdc_address = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
 }
 
 describe("uniswap logic happy path tests", function () {
-  it.only("runs", async function () {
+  it("runs", async function () {
     await runTest();
   });
 });
