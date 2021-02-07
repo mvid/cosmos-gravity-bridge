8350af1f5f589ebda0652cb6dc424e94d7caa0fe
Justin Kilpatrick <justin@althea.net>
2021-02-07 07:40:50 -0500

    Use four space indentation on TestUniswapLiquidity

--- solidity/contracts/TestUniswapLiquidity.sol
+++ solidity/contracts/TestUniswapLiquidity.sol
@@ -9,33 +9,33 @@ import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 
 contract TestUniswapLiquidity is Ownable {
-        address router;
+    address router;
 
-        constructor(address _uni_router) public {
-                router = _uni_router;
-        }
+    constructor(address _uni_router) public {
+        router = _uni_router;
+    }
 
-        function redeemLiquidityETH(
-        address token,
-        uint liquidity,
-        uint amountTokenMin,
-        uint amountETHMin,
-        address to,
-        uint deadline
-        ) public onlyOwner {
-                IUniswapV2Router02(router).removeLiquidityETH(token,liquidity,amountTokenMin,amountETHMin,to,deadline);
-        }
+    function redeemLiquidityETH(
+    address token,
+    uint liquidity,
+    uint amountTokenMin,
+    uint amountETHMin,
+    address to,
+    uint deadline
+    ) public onlyOwner {
+        IUniswapV2Router02(router).removeLiquidityETH(token,liquidity,amountTokenMin,amountETHMin,to,deadline);
+    }
 
 
-        function redeemLiquidity(
-        address tokenA,
-        address tokenB,
-        uint liquidity,
-        uint amountAMin,
-        uint amountBMin,
-        address to,
-        uint deadline
-        ) public onlyOwner {
-                IUniswapV2Router02(router).removeLiquidity(tokenA,tokenB,liquidity,amountAMin,amountBMin,to,deadline);
-        }
+    function redeemLiquidity(
+    address tokenA,
+    address tokenB,
+    uint liquidity,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+    ) public onlyOwner {
+        IUniswapV2Router02(router).removeLiquidity(tokenA,tokenB,liquidity,amountAMin,amountBMin,to,deadline);
+    }
 }
