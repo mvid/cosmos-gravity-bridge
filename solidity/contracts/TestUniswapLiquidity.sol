58a1f16beaed16cde17b2c17fef5bf656af90327
Zaki Manian <zaki@manian.org>
2021-02-07 21:49:16 -0800

    Work in progress on improving tests

--- solidity/contracts/TestUniswapLiquidity.sol
+++ solidity/contracts/TestUniswapLiquidity.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
 
 contract TestUniswapLiquidity is Ownable {
